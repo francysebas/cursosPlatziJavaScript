8a4d25fe71cb59d3d0a58b66452ceebfa1ba4e5f
francysebas <francyn@unicauca.edu.co>
2022-08-24 17:51:07 -0500

    otro js

--- CursoPracticoJavaScript/script.js
+++ CursoPracticoJavaScript/script.js
@@ -10,9 +10,9 @@ const h1=document.querySelector('h1');
 const p = document.querySelector('p');
 const input = document.querySelector('input');
 //seleccionar la clase
-const parrafito = document.getElementsByClassName('.parrafito');
+const parrafito = document.getElementsByClassName('parrafito');
 //seleccionar  id
-const pid = document.getElementById('#pid');
+const pid = document.getElementById('pid');
 
 console.log(input.value);
 
@@ -27,6 +27,26 @@ console.log({
 //manejo del html desde javaCript
 //h1.innerHTML = 'cambio... <br> desde javaScript...';
 
-h1.innerText = 'cambio... <br> desde javaScript...';
-
 console.log(h1.getAttribute('class'));
+
+//modifica los atributos de la clase
+h1.setAttribute('class', 'azul');
+
+//Adicionar per con clases, tambien se puede el remove
+h1.classList.add('rojo');
+
+//toggle  contains
+
+//cambiar un input desde js
+input.value = "Digita un nombre"
+
+//crear un elemento desde cero
+// console.log(document.createElement('img'));
+// console.log(document.createElement('span'));
+
+const img = document.createElement('img');
+img.setAttribute('src','https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2022/05/avatar-2-disney.jpg?fit=2500%2C1406&quality=50&strip=all&ssl=1');
+console.log(img);
+
+// pid.innerHTML= "";
+pid.append(img);
---
+++ CursoPracticoJavaScript2/script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1');
+//const input1 = document.querySelector('#calculo1');
+const input1 = document.querySelector('#calculo')
+const input2 = document.querySelector('#calculo2');
+const btnCalcular = document.querySelector('#btnCalcular');
+
+function btnOnClick(){
+    
+    console.log(input1.value + input2.value);
+}
